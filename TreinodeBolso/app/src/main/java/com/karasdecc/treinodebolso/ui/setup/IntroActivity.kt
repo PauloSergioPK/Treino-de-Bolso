757e0ca25258182eee35bbb4d198cba80abb546e
Paulo Sergio Rabelo Costa <40449931+PauloSergioPK@users.noreply.github.com>
2021-03-19 02:01:58 -0300

    inicio do desenvolvimento do login

---
+++ TreinodeBolso/app/src/main/java/com/karasdecc/treinodebolso/ui/setup/IntroActivity.kt
@@ -0,0 +1,17 @@
+package com.karasdecc.treinodebolso.ui.setup
+
+import android.os.Bundle
+import com.karasdecc.treinodebolso.databinding.ActivityIntroBinding
+import com.karasdecc.treinodebolso.ui.BaseBindingActivity
+
+class IntroActivity : BaseBindingActivity<ActivityIntroBinding>(ActivityIntroBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupViews()
+    }
+
+    private fun setupViews(){
+
+    }
+}
