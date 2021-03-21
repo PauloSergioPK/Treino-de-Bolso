24a7fa93e1b9e98bb3a744c196c00841c36de379
Seph47 <nathanfs710@gmail.com>
2021-03-21 16:26:15 -0300

    Para tu, meu lindinho, uma bela classe, amém!!

---
+++ TreinodeBolso/app/src/main/java/com/karasdecc/treinodebolso/ui/main/activities/ExerciciosActivity.kt
@@ -0,0 +1,11 @@
+package com.karasdecc.treinodebolso.ui.main.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExerciciosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercicios)
+    }
+}
